9e07c8550fe77507a3eb92ca66c214e1314a4984
g000001 <chiba.masaomi@gmail.com>
2013-01-23 08:23:34 +0900

    rm readtable.lisp

--- readtable.lisp
+++
@@ -1,9 +0,0 @@
-;;;; readtable.lisp
-
-(cl:in-package :ai.lang.lisp.code.parsing.time.internal)
-(in-readtable :common-lisp)
-
-#|(defreadtable :ai.lang.lisp.code.parsing.time  (:merge :standard)
-  (:macro-char char fctn opt...)
-  (:syntax-from readtable to-char from-char)
-  (:case :upcase))|#
