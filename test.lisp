7191c4f2a256f0e348750278582f8c4361af7115
g000001 <chiba.masaomi@gmail.com>
2013-01-23 08:17:19 +0900

    first commit.

--- test.lisp
+++ test.lisp
@@ -13,11 +13,8 @@
   (is (<= (get-universal-time) (parse-time "now") (get-universal-time)))
   (is (= (parse-time "Mon, 1 Jan 1900 00:00:00 0000")
          0))
-  (is (multiple-value-bind (s m h d mo y) 
-                           (decode-universal-time (get-universal-time) 0)
-        (declare (ignore s m h))
-        (= (encode-universal-time 0 0 12 d mo y 0)
-           (parse-time "noon" :default-zone 0)))))
+  (is (= (parse-time "1900/1/1 noon" :default-zone 0)
+         (encode-universal-time 0 0 12 1 1 1900 0))))
 
 
 ;;; *EOF* 
